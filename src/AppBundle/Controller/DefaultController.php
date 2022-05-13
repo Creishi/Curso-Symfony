1272bff2d8531795bf12dd35867789154924f333
root <cristianfabiant2@gmail.com>
2022-05-13 09:16:02 -0500

    Inicio del proyecto creación de 3 action, cración menu de navegación estatico y creación de vista nosotros.

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig');
+    }
+
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotrosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/nosotros.html.twig');
+    }
+
+    /**
+     * @Route("/contactar", name="contactar")
+     */
+    public function contactarAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/bares.html.twig');
+    }
+}
